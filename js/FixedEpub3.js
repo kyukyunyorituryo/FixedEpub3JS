6f109aa151269c118b0c845d9e07615df1b544e0
kyukyunyorituryo <kyukyunyorituryo@live.jp>
2017-04-09 14:32:41 +0900

    テンプレートファイルを入力内容から書き換えられるように変更 zipファイル読み込みの除去

--- js/FixedEpub3.js
+++ js/FixedEpub3.js
@@ -1,9 +1,14 @@
 var imgfile= [];
+//テスト用
+var k=[];
+
   function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
 
     // Loop through the FileList and render image files as thumbnails.
     for (var i = 0, f; f = files[i]; i++) {
+//テスト用
+k.push(f);
 
       // Only process image files.
       if (!f.type.match('image.*')) {
@@ -30,68 +35,8 @@ var imgfile= [];
   }
 document.addEventListener("DOMContentLoaded", function(){
   document.getElementById('files').addEventListener('change', handleFileSelect, false);});
-  
-
-(function () {
-  if (!window.FileReader || !window.ArrayBuffer) {
-    $("#error_block").removeClass("hidden").addClass("show");
-    return;
-  }
 
 
-  var $result = $("#result");
-  $("#file").on("change", function(evt) {
-    // remove content
-    $result.html("");
-    // be sure to show the results
-    $("#result_block").removeClass("hidden").addClass("show");
-
-    // Closure to capture the file information.
-    function handleFile(f) {
-      var $title = $("<h4>", {
-        text : f.name
-      });
-      var $fileContent = $("<ul>");
-      $result.append($title);
-      $result.append($fileContent);
-    
-      var dateBefore = new Date();
-      JSZip.loadAsync(f)
-      .then(function(zip) {
-        var dateAfter = new Date();
-        $title.append($("<span>", {
-          text:" (loaded in " + (dateAfter - dateBefore) + "ms)"
-        }));
-
-
-        zip.forEach(function (relativePath, zipEntry) {
-          $fileContent.append($("<li>", {
-            text : zipEntry.name
-          }));
-        });
-      }, function (e) {
-        $fileContent = $("<div>", {
-          "class" : "alert alert-danger",
-          text : "Error reading " + f.name + " : " + e.message
-        });
-      });
-    }
-
-    var files = evt.target.files;
-    for (var i = 0, f; f = files[i]; i++) {
-        handleFile(f);
-    }
-  });
-})();
-
-
-      // FIXME find how to do that cleanly
-      (function(){
-        var tables = document.getElementsByTagName("table");
-        for(var i = 0; i < tables.length; i++) {
-          tables[i].className += " table table-condensed table-striped table-bordered ";
-        }
-      })();
 //EPUB３テンプレート
 var containerXML ='<?xml version="1.0" encoding="UTF-8"?>\n<container xmlns="urn:oasis:names:tc:opendocument:xmlns:container" version="1.0">\n<rootfiles>\n<rootfile full-path="item/standard.opf" media-type="application/oebps-package+xml"/>\n</rootfiles>\n</container>';
 var standardOPF = '<?xml version="1.0" encoding="utf-8"?>\n<package xmlns="http://www.idpf.org/2007/opf" version="3.0" xml:lang="ja" unique-identifier="unique-id" prefix="rendition: http://www.idpf.org/vocab/rendition/# ebpaj: http://www.ebpaj.jp/" >\n<metadata xmlns:dc="http://purl.org/dc/elements/1.1/">\n<!-- 作品名 -->\n<dc:title id="title">作品名１</dc:title>\n<meta refines="#title" property="file-as">セイレツヨウサクヒンメイカナ01</meta>\n<!-- 著者名 -->\n<dc:creator id="creator01">著作者名１</dc:creator>\n<meta refines="#creator01" property="role" scheme="marc:relators">aut</meta>\n<meta refines="#creator01" property="file-as"> セイレツヨウチョサクシャメイカナ 01</meta>\n<meta refines="#creator01" property="display-seq">1</meta>\n<dc:creator id="creator02">著作者名２</dc:creator>\n<meta refines="#creator02" property="role" scheme="marc:relators">aut</meta>\n<meta refines="#creator02" property="file-as"> セイレツヨウチョサクシャメイカナ 02</meta>\n<meta refines="#creator02" property="display-seq">2</meta>\n<!-- 出版社名 -->\n<dc:publisher id="publisher">出版社名</dc:publisher>\n<meta refines="#publisher" property="file-as"> セイレツヨウシュッパンシャメイカナ</meta>\n<!-- 言語 -->\n<dc:language>ja</dc:language>\n<!-- ファイルid -->\n<dc:identifier id="unique-id">urn:uuid:860ddf31-55a4-449a-8cc9-3c1837657a15</dc:identifier>\n<!-- 更新日 -->\n<meta property="dcterms:modified">2012-01-01T00:00:00Z</meta>\n<!-- Fixed-Layout Documents指定 -->\n<meta property="rendition:layout">pre-paginated</meta>\n<meta property="rendition:spread">landscape</meta>\n<!-- etc. -->\n<meta property="ebpaj:guide-version">1.1</meta>\n</metadata>\n<manifest>\n<!-- navigation -->\n<item media-type="application/xhtml+xml" id="toc" href="nav.xhtml" properties="nav"/>\n<!-- style -->\n<item media-type="text/css" id="fixed-layout-jp" href="style/fixed-layout-jp.css"/>\n<!-- image -->\n<item media-type="image/jpeg" id="cover" href="image/cover.jpg" properties="cover-image"/>\n<item media-type="image/jpeg" id="i-001" href="image/i-001.jpg"/>\n<!-- xhtml -->\n<item media-type="application/xhtml+xml" id="p-cover" href="xhtml/p-cover.xhtml" properties="svg" fallback="cover"/>\n<item media-type="application/xhtml+xml" id="p-001" href="xhtml/p-001.xhtml" properties="svg" fallback="i-001"/>\n</manifest>\n<spine page-progression-direction="rtl">\n<itemref linear="yes" idref="p-cover" properties="rendition:page-spread-center"/>\n<itemref linear="yes" idref="p-001" properties="page-spread-right"/>\n</spine>\n</package>';
@@ -111,16 +56,58 @@ var layout='@charset "UTF-8";\n\nhtml,\nbody {\n  margin:    0;\n  padding:   0;
 function rewrite(){
 var standardOPFxml = (new DOMParser()).parseFromString(standardOPF, 'text/xml');
 //タイトル
-standardOPFxml.querySelector('title').textContent=$("#title").val();
+standardOPFxml.getElementById('title').textContent=$("#title").val();
+var sttitle=standardOPFxml.querySelector("meta[refines='#title']");
+//sttitle.parentNode.removeChild(sttitle);
 //著者１
+standardOPFxml.getElementById('creator01').textContent=$("#author1").val();
+var metaall = standardOPFxml.querySelectorAll("meta[refines='#creator01']");
+for  (i = 0; i < metaall.length; i++){
+metaall[i].parentNode.removeChild(metaall[i]);}
 //著者２
+standardOPFxml.getElementById('creator02').textContent=$("#author2").val();
+var metaall = standardOPFxml.querySelectorAll("meta[refines='#creator02']");
+for  (i = 0; i < metaall.length; i++){
+metaall[i].parentNode.removeChild(metaall[i]);}
+//出版社
+//ファイルID、uuid
+//日時
+var today = new Date();
+standardOPFxml.querySelector("meta[property='dcterms:modified']").textContent=today.toISOString();
+//XMLシリアライズ
 standardOPF = (new XMLSerializer()).serializeToString(standardOPFxml);
 console.log(standardOPFxml);
+
 //ナビゲーションファイル
 var navigationXml = (new DOMParser()).parseFromString(navigation, 'text/xml');
-navigationXml.querySelector('title').textContent=$("#title").val();
+
 navigation = (new XMLSerializer()).serializeToString(navigationXml);
+
+
+//表紙XHTML　coverxhtml
+var coverxhtmlXml = (new DOMParser()).parseFromString(coverxhtml, 'text/xml');
+coverxhtmlXml.querySelector('title').textContent=$("#title").val();
+var viewport = coverxhtmlXml.querySelector("meta[content]");
+var svg = coverxhtmlXml.querySelector("svg[viewBox]");
+var imagesize = coverxhtmlXml.querySelector("image");
+viewport.setAttribute("content", 'width='+$("#imgwidth").val() +' ,'+'height='+$("#imgheight").val());
+svg.setAttribute("viewBox", '0 0 '+$("#imgwidth").val() +' '+$("#imgheight").val());
+imagesize.setAttribute("width",$("#imgwidth").val());
+imagesize.setAttribute("height",$("#imgheight").val());
+//画像ファイル名の設定
+//imagesize.setAttributeNS("http://www.w3.org/1999/xlink","href","../image/"+"test.jpg");
+
+coverxhtml = (new XMLSerializer()).serializeToString(coverxhtmlXml);
+console.log(viewport.content);
+console.log(coverxhtmlXml);
+console.log(imagesize.getAttributeNS("http://www.w3.org/1999/xlink","href"));
+
+//ページXHTML　pagexhtml
+var pagexhtmlXml = (new DOMParser()).parseFromString(pagexhtml, 'text/xml');
+
+pagexhtml = (new XMLSerializer()).serializeToString(pagexhtmlXml);
 }
+
 //zip圧縮
 jQuery(function($) {
   if(!JSZip.support.blob) {
