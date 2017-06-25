4ac12bc81c4f6b4bd4e8c314356dd9016b990f10
kyukyunyorituryo <kyukyunyorituryo@live.jp>
2017-06-25 23:58:08 +0900

    整列機能をつけた

--- js/FixedEpub3.js
+++ js/FixedEpub3.js
@@ -79,6 +79,7 @@ document.addEventListener("DOMContentLoaded", function(){
           span.innerHTML = ['<img class="thumb" src="', e.target.result,
                             '" title="', theFile.name, '"/>'].join('');
           document.getElementById('list').insertBefore(span, null);
+        imgFO.push({file_name:theFile.name,data:e.target.result,type:theFile.type});
 //チェックコード
 //        var image =new Image();
 //          image.src = e.target.result;
@@ -88,7 +89,6 @@ document.addEventListener("DOMContentLoaded", function(){
 //};
 //console.log(theFile.name);
 //console.log(theFile.type);
-imgFO.push({file_name:theFile.name,data:e.target.result,type:theFile.type});
         };
       })(f);
 
@@ -98,7 +98,39 @@ imgFO.push({file_name:theFile.name,data:e.target.result,type:theFile.type});
   }
 document.addEventListener("DOMContentLoaded", function(){
   document.getElementById('files').addEventListener('change', handleFileSelect, false);});
+//sort　本文画像の整列
+function mySort() {
+var list = document.getElementById('list');
+var Nlist=list.getElementsByTagName('span');
+var myArray = Array.prototype.slice.call(Nlist);
+   function compareText (a,b) {
+        if (a.firstChild.title > b.firstChild.title)
+            return 1;
+        else if (a.firstChild.title < b.firstChild.title)
+            return -1;
+        return 0;
+        }
+    myArray.sort(compareText);
+       for (var i=0; i<myArray.length; i++) {
+        list.appendChild(list.removeChild(myArray[i]))
+    }
 
+//画像ファイル名で整列
+//imgFO = [{file_id:"",file_name:'cover.jpg',data:'',type:'image/jpeg'}];
+ imgFO.sort(function(a,b){
+    if(a.file_name<b.file_name) return -1;
+    if(a.file_name > b.file_name) return 1;
+    return 0;
+});
+
+}
+//目次の追加
+function addmenu(){
+var menu = document.getElementsByTagName("form")[1]
+var cmenu= menu.cloneNode(true);
+var par=document.getElementById("bg");
+par.insertBefore(cmenu,menu.nextSibling);
+}
 //EPUB３テンプレート
 var containerXML ='<?xml version="1.0" encoding="UTF-8"?>\n<container xmlns="urn:oasis:names:tc:opendocument:xmlns:container" version="1.0">\n<rootfiles>\n<rootfile full-path="item/standard.opf" media-type="application/oebps-package+xml"/>\n</rootfiles>\n</container>';
 var standardOPF = '<?xml version="1.0" encoding="utf-8"?>\n<package xmlns="http://www.idpf.org/2007/opf" version="3.0" xml:lang="ja" unique-identifier="unique-id" prefix="rendition: http://www.idpf.org/vocab/rendition/# ebpaj: http://www.ebpaj.jp/" >\n<metadata xmlns:dc="http://purl.org/dc/elements/1.1/">\n<!-- 作品名 -->\n<dc:title id="title">作品名１</dc:title>\n<meta refines="#title" property="file-as">セイレツヨウサクヒンメイカナ01</meta>\n<!-- 著者名 -->\n<dc:creator id="creator01">著作者名１</dc:creator>\n<meta refines="#creator01" property="role" scheme="marc:relators">aut</meta>\n<meta refines="#creator01" property="file-as"> セイレツヨウチョサクシャメイカナ 01</meta>\n<meta refines="#creator01" property="display-seq">1</meta>\n<dc:creator id="creator02">著作者名２</dc:creator>\n<meta refines="#creator02" property="role" scheme="marc:relators">aut</meta>\n<meta refines="#creator02" property="file-as"> セイレツヨウチョサクシャメイカナ 02</meta>\n<meta refines="#creator02" property="display-seq">2</meta>\n<!-- 出版社名 -->\n<dc:publisher id="publisher">出版社名</dc:publisher>\n<meta refines="#publisher" property="file-as"> セイレツヨウシュッパンシャメイカナ</meta>\n<!-- 言語 -->\n<dc:language>ja</dc:language>\n<!-- ファイルid -->\n<dc:identifier id="unique-id">urn:uuid:860ddf31-55a4-449a-8cc9-3c1837657a15</dc:identifier>\n<!-- 更新日 -->\n<meta property="dcterms:modified">2012-01-01T00:00:00Z</meta>\n<!-- Fixed-Layout Documents指定 -->\n<meta property="rendition:layout">pre-paginated</meta>\n<meta property="rendition:spread">landscape</meta>\n<!-- etc. -->\n<meta property="ebpaj:guide-version">1.1</meta>\n</metadata>\n<manifest>\n<!-- navigation -->\n<item media-type="application/xhtml+xml" id="toc" href="nav.xhtml" properties="nav"/>\n<item media-type="application/x-dtbncx+xml" id="ncxtoc" href="toc.ncx"/>\n<!-- style -->\n<item media-type="text/css" id="fixed-layout-jp" href="style/fixed-layout-jp.css"/>\n<!-- image -->\n<item media-type="image/jpeg" id="cover" href="image/cover.jpg" />\n<!-- xhtml -->\n<item media-type="application/xhtml+xml" id="p-cover" href="xhtml/p-cover.xhtml" properties="svg" fallback="cover"/>\n</manifest>\n<spine toc="ncxtoc" page-progression-direction="rtl">\n<itemref linear="yes" idref="p-cover" properties="rendition:page-spread-center"/>\n</spine>\n</package>';
@@ -120,13 +152,7 @@ function rewriteOPF(){
 //キンドルの場合kindleOPF、それ以外はstandardOPFに設定する。
 //var flag = document.getElementById("radio1").checked;
 //if(flag){standardOPF=kindleOPF};
-//画像ファイル名で整列
-//imgFO = [{file_id:"",file_name:'cover.jpg',data:'',type:'image/jpeg'}];
- imgFO.sort(function(a,b){
-    if(a.file_name<b.file_name) return -1;
-    if(a.file_name > b.file_name) return 1;
-    return 0;
-});
+
 
 //OPFファイルの書換
 var standardOPFxml = (new DOMParser()).parseFromString(standardOPF, 'text/xml');
